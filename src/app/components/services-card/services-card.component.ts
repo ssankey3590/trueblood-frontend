0868b570cfe412d685a6dfaea0adbdbfd1bc9f93
Stewart Sankey <stewart-sankey@uiowa.edu>
2024-08-26 23:06:30 -0500

    added services list

--- src/app/components/services-card/services-card.component.ts
+++ src/app/components/services-card/services-card.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {MatCard,MatCardTitle, MatCardContent, MatCardFooter, MatCardHeader} from "@angular/material/card";
 import {MatList, MatListItem} from "@angular/material/list";
+import {MatChip, MatChipListbox} from "@angular/material/chips";
 
 @Component({
   selector: 'app-services-card',
@@ -12,7 +13,9 @@ import {MatList, MatListItem} from "@angular/material/list";
     MatCardContent,
     MatCardFooter,
     MatList,
-    MatListItem
+    MatListItem,
+    MatChipListbox,
+    MatChip
   ],
   templateUrl: './services-card.component.html',
   styleUrl: './services-card.component.scss'
