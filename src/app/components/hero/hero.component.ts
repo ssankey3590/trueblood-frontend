a56135dfadde3556251bd0b26667aae455daae5e
Stewart Sankey <ssankey@stewartsankey.com>
2024-10-05 05:00:33 -0500

    updating info

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import {PageTemplateComponent} from "../../components/page-template/page-template.component";
+import {MatIcon} from "@angular/material/icon";
+import {MatList, MatListItem} from "@angular/material/list";
+import {MatButton, MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-    imports: [
-        PageTemplateComponent
-    ],
+  imports: [
+    PageTemplateComponent,
+    MatIcon,
+    MatList,
+    MatListItem,
+    MatIconButton,
+    MatButton
+  ],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
