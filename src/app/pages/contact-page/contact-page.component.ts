70569790c8cd7d20b1a9acecb9a78c07335ed5c7
Stewart Sankey <ssankey@stewartsankey.com>
2024-10-05 05:23:51 -0500

    updating services

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -3,6 +3,9 @@ import {PageTemplateComponent} from "../../components/page-template/page-templat
 import {MatIcon} from "@angular/material/icon";
 import {MatList, MatListItem} from "@angular/material/list";
 import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {Services, SERVICES} from "../../interfaces/services.interface";
 
 @Component({
   selector: 'app-contact-page',
@@ -11,13 +14,18 @@ import {MatButton, MatIconButton} from "@angular/material/button";
     PageTemplateComponent,
     MatIcon,
     MatList,
+    MatLabel,
     MatListItem,
     MatIconButton,
-    MatButton
+    MatButton,
+    MatFormField,
+    MatSelect,
+    MatOption
   ],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
+  protected readonly services: Services[] = SERVICES;
 
 }
--- src/app/pages/services-page/services-page.component.ts
+++ src/app/pages/services-page/services-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {ServicesCardComponent} from "../../components/services-card/services-card.component";
 import {PageTemplateComponent} from "../../components/page-template/page-template.component";
-import {Services} from "../../interfaces/services.interface";
+import {SERVICES} from "../../interfaces/services.interface";
 
 @Component({
   selector: 'app-services-page',
@@ -14,93 +14,7 @@ import {Services} from "../../interfaces/services.interface";
   styleUrl: './services-page.component.scss'
 })
 export class ServicesPageComponent {
-  services: Services[] = [
-    {
-      name: 'Furniture Assembly',
-      description: '',
-      keywords: ['Desks','Shelves','Chairs','Tables','Beds']
-    },
-    {
-      name: 'Drywall Repair and Maintenance',
-      description: '',
-      keywords: ['Hole patching','Nail pops', 'Wallpaper removal']
-    },
-    {
-      name: 'Minor Plumbing',
-      description: '',
-      keywords: ['Faucet leaks','Shower head installation','Faucet head installation','Caulking']
-    },
-    {
-      name: 'Minor Electrical',
-      description: '',
-      keywords: ['Home security installation','Smoke detector','Carbon detector', 'Upgrades', 'Replacement']
-    },
-    {
-      name: 'Baby Proofing',
-      description: '',
-      keywords: ['Outlet covers', 'Safety locks', 'Baby gates', 'Anchoring furniture']
-    },
-    {
-      name: 'Light Fixture Installation',
-      description: '',
-      keywords: ['Track lighting','Ceiling fans','Cabinet lighting', 'Recessed lighting']
-    },
-    {
-      name: 'Pressure Washing',
-      description: '',
-      keywords: ['Decks','Stairs','Driveways','Exteriors']
-    },
-    {
-      name: 'Smart Home Upgrades',
-      description: '',
-      keywords: ['Appliances','Doorbells','Thermostats']
-    },
-    {
-      name: 'Fence and Gate Repair',
-      description: '',
-      keywords: ['Gate installation','Fence panels and posts repairs','Latch repairs','Decorative fences']
-    },
-    {
-      name: 'Window Coverings',
-      description: '',
-      keywords: ['Blinds','Curtains']
-    },
-    {
-      name: 'Door Hardware Installation',
-      description: '',
-      keywords: ['Doorknobs','Deadbolts','Locks','Door handles']
-    },
-    {
-      name: 'Senior Home Upgrades',
-      description: '',
-      keywords: ['Walk-in tub','Grab bar installation','Stair lift installation']
-    },
-    {
-      name: 'Gutter Cleaning',
-      description: '',
-      keywords: ['Debris removal','Clogs','Gutter repairs','Downspout cleaning']
-    },
-    {
-      name: 'Window Washing',
-      description: '',
-      keywords: ['Two-story windows','Three-story windows','Ground floor windows']
-    },
-    {
-      name: 'Screen Installation and Repair',
-      description: '',
-      keywords: ['Frame repairs','Tear patching','Screen replacement']
-    },
-    {
-      name: 'Small Appliances Installation and Repair',
-      description: '',
-      keywords: ['Microwaves','Washing machines','Dryers','Dishwashers']
-    },
-    {
-      name: 'Picture / TV Hanging and Installation',
-      description: '',
-      keywords: ['TV Mounting','Pictures','Mirrors','Paintings']
-    },
-  ];
 
 
+  protected readonly services = SERVICES;
 }
